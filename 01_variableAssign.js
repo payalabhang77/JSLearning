e79b838f40fe44f7180989f722727759105b225e
payalabhang77 <144082506+payalabhang77@users.noreply.github.com>
2023-09-20 13:03:07 +0530

    variable assign
    variable assign

---
+++ 01_variableAssign.js
@@ -0,0 +1,30 @@
+var myFullName
+console.log(myFullName);
+var myFullName = "Payal Abhang";
+console.log(myFullName);
+var myFullName = "Payal Shrikant Abhang";
+console.log(myFullName);
+
+var myLove="I Love Only JS";
+console.log(myLove);
+
+var firstName = "Payal"
+console.log("First Name :", firstName);
+
+var lastName = "Abhang";
+console.log("Last Name :", lastName);
+
+var age = "23";
+console.log("Age :", age);
+
+var address = "Pune";
+console.log("Address :", address);
+
+var pinCode = "413132";
+console.log("PinCode :", pinCode);
+
+var gender = "Female";
+console.log("Gender :", gender);
+
+var isMarried = "Yes";
+console.log("IsMarried :", isMarried);
