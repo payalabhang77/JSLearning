a9198f8d87a9825b4ffb8abf0421cdac8ac6fcf4
payalabhang77 <144082506+payalabhang77@users.noreply.github.com>
2023-09-21 20:21:31 +0530

    assign typeof
    assign typeof

---
+++ 02_typeOf.js
@@ -0,0 +1,34 @@
+console.log("=================Assignment 2=======================");
+
+console.log("----------------step 1----------------------");
+
+var bankName;
+console.log("before initialization==>",bankName,"Type ==>",typeof bankName);
+
+var bankName="SBI Bank";
+console.log("After initialization ==>",bankName,"Type ==>",typeof bankName);
+
+console.log("---------------------step 2---------------------------");
+
+var marks = "90%";
+console.log( "Marks ==>",marks,"Type ==>",typeof  marks  );
+
+console.log("-------------------step 3-----------------------------");
+
+var isWorking;
+console.log("before initialization==>",isWorking,"Type ==>",typeof isWorking);
+
+var  isWorking = true;
+console.log( "After initialization  ==>",isWorking,"Type ==>",typeof  isWorking);
+
+console.log("-------------------step 4-------------------------");
+
+console.log("-------------------a-----------------------");
+
+var totalCount="Hundred and Seven";
+console.log("Total Count ==>",totalCount,"Type ==>",typeof totalCount);
+
+console.log("------------------b----------------------");
+
+var totalCount=107;
+console.log("Total Count ==>",totalCount,"Type ==>",typeof totalCount);
