a4db0054e289c047d1a774ab7e6845303f9ef335
payalabhang77 <144082506+payalabhang77@users.noreply.github.com>
2023-09-21 20:20:02 +0530

    variable basic assign
     variable basic assign

---
+++ 02_variableBasicAssign.js
@@ -0,0 +1,45 @@
+console.log("=================Assignment 1=================");
+
+console.log("------------------Step 1--------------------");
+
+console.log('--------------------string---------------------');
+
+var name="Payal Abhang";
+console.log(" Name ==>",name,"Type ==>",typeof name);
+var favoriteSubject="JavaScript";
+console.log("Favorite Subject ==>",favoriteSubject,"Type ==>",typeof favoriteSubject);
+var country="India";
+console.log("Country ==>" ,country,"Type ==>",typeof  country);
+
+console.log("-------------------number----------------------");
+
+var pinCode = 413132;
+console.log(" PinCode ==>" ,pinCode,"Type ==>",typeof  pinCode);
+var age = 23;
+console.log( "Age ==>",age,"Type ==>",typeof age);
+var mobileNo = 8867789907;
+console.log("Mobile Number ==>",mobileNo,"Type ==>",typeof mobileNo);
+
+console.log("----------------------boolean-------------------------");
+
+var isActive = true;
+console.log("Is Active ==>",isActive,"Type ==>",typeof isActive);
+var isPassed = true;
+console.log("Is Passed ==>",isPassed,"Type ==>",typeof isPassed);
+var isHandicap = false;
+console.log("Is Handicap ==>",isHandicap,"Type ==>",typeof isHandicap);
+
+ console.log("--------------------step2------------------");
+
+ console.log("-----------------null vs undefined--------------------");
+
+ var mobileNumber;
+ console.log("undefined means variable is not initialized ==>", mobileNumber,"Type ==>",typeof mobileNumber);
+
+ var mobileNumber = null ;
+ console.log(" null means there is no value it is empty==>", mobileNumber,"Type ==>",typeof mobileNumber);
+
+
+ 
+
+
