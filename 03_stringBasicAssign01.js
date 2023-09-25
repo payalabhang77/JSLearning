6c17050dbe086c489a9d6970a3d27b0baf7f3092
payalabhang77 <144082506+payalabhang77@users.noreply.github.com>
2023-09-25 14:29:59 +0530

    string basic assign01
    string basic assign01

---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,33 @@
+
+console.log("=========================Assignment stringBasicAssign01=============================");
+
+function stringBasic() {
+    console.log("My Dream Company  is: Infosys");
+}
+stringBasic()
+
+console.log("--------------------------------------------------------");
+
+var hobby1="Programming";
+var hobby2="Traveling";
+var hobby3="Cooking";
+
+console.log("My Hobbies is :",hobby1,hobby2,hobby3);
+
+console.log("-----------------------------------------------------------");
+
+var str="programming,traveling,cooking";
+var totalLength =str.length;
+console.log("total number of character :",totalLength);
+
+console.log("---------------------------------------------------------");
+
+
+
+
+
+
+
+
+
+
