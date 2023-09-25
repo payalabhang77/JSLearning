d085cb431a9f2a8b8981490488bef6112ef55850
payalabhang77 <144082506+payalabhang77@users.noreply.github.com>
2023-09-25 14:32:28 +0530

    fun assign
    fun assign

---
+++ 03_funAssign02.js
@@ -0,0 +1,12 @@
+
+console.log("=====================================Assignment funAssign02==============================================");
+
+function bankDetails(bankName,accountNum,location,pinCode) {
+    console.log("Bank Name : CITI Bank"  ,"account Number :",3456782345 ,"Location : Pune"   ,"Pin Code :",431202);
+    console.log("---------------------------------------------------------------------------------------------------");
+    console.log("Bank Name : Axis Bank"  ,"account Number :",7856782345 ,"Location : Mumbai" ,"Pin Code :",441202);
+    console.log("---------------------------------------------------------------------------------------------------");
+    console.log("Bank Name : HDFC Bank"  ,"account Number :",8956782345 ,"Location : Pune"   ,"Pin Code :",631202);
+    console.log("---------------------------------------------------------------------------------------------------");
+}
+bankDetails();
