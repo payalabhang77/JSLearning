cd303d5f67479fe206fd91b3eaa3b9d7a12f7151
payalabhang77 <144082506+payalabhang77@users.noreply.github.com>
2023-09-26 08:33:37 +0530

    string basic assign
    string basic assign

--- 03_stringBasicAssign01.js
+++ 03_stringBasicAssign01.js
@@ -3,25 +3,28 @@ console.log("=========================Assignment stringBasicAssign01============
 
 function stringBasic() {
     console.log("My Dream Company  is: Infosys");
+
+    console.log("--------------------------------------------------------");
+
+ var hobby1="Programming";
+ var hobby2="Traveling";
+ var hobby3="Cooking";
+
+ console.log("My Hobbies is :",hobby1,hobby2,hobby3);
+
+ console.log("-----------------------------------------------------------");
+
+ var str="programming,traveling,cooking";
+ var totalLength =str.length;
+ console.log("total number of character :",totalLength);
+
+ console.log("---------------------------------------------------------");
+
 }
-stringBasic()
+stringBasic();
 
-console.log("--------------------------------------------------------");
-
-var hobby1="Programming";
-var hobby2="Traveling";
-var hobby3="Cooking";
-
-console.log("My Hobbies is :",hobby1,hobby2,hobby3);
-
-console.log("-----------------------------------------------------------");
-
-var str="programming,traveling,cooking";
-var totalLength =str.length;
-console.log("total number of character :",totalLength);
-
-console.log("---------------------------------------------------------");
-
+    
+   
 
 
 
