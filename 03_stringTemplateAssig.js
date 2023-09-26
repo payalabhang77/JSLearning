b20d397bea35708e1124b6d646d2080d0d77ce6d
payalabhang77 <144082506+payalabhang77@users.noreply.github.com>
2023-09-26 14:09:22 +0530

    string template assign
    string template assign

---
+++ 03_stringTemplateAssig.js
@@ -0,0 +1,22 @@
+console.log(`====================Assignment 1=======================`);
+
+function stringTemplateAssign() {
+
+    console.log(`-----------------step 1----------------------------`);
+
+    console.log(`My Dream Company Name is : "Infosys"`);
+
+    console.log(`--------------------step 2------------------------------`);
+
+    var hobby1 = "programming";
+    var hobby2 = "reading";
+    var hobby3 = "cooking";
+
+    console.log(`My Hobbies are : ${hobby1}, ${hobby2}, ${hobby3}`);
+
+    console.log(`--------------------------------------------------`);
+
+    var hobbies = "programming reading cooking";
+    console.log(`concat hobbies in one string : ${hobbies}`);
+}
+stringTemplateAssign();
