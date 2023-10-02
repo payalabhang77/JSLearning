27156143e8e65b48af3e862970a6853367657bf5
payalabhang77 <144082506+payalabhang77@users.noreply.github.com>
2023-10-02 22:13:49 +0530

    for loop assign
    for loop assign

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,48 @@
+console.log(`=========================Assignment=================================`);
+
+console.log(`1.-----------5 to 15 by increment 1--------------`);
+
+for (let index = 5; index <=15; index++) {
+    console.log(`${index}`);
+}
+
+console.log(`2.--------------50 to 40 by decrement 1-----------------`);
+    
+for (let index = 50; index >=40; index=index-1) {
+    console.log(`${index}`);
+}
+
+console.log(`3.--------------First 15 odd numbers--------------`);
+
+for (let index = 1; index <=30; index=index+2) {
+    console.log(`${index}`);
+    
+}
+
+console.log(`4.--------------first 10 even numbers----------------`);
+ 
+for (let index = 2; index <=20; index=index+2) {
+    console.log(`${index}`);
+    
+}
+
+console.log(`5.-------------5 table-------------`);
+
+for (let index = 5; index <=50; index=index+5) {
+    console.log(`${index}`);
+    
+}
+
+console.log(`6.-----------10 table------------`);
+
+for (let index = 10; index <=100; index=index+10) {
+    console.log(`${index}`);
+    
+}
+
+console.log(`7.-----------10 table - reverse order-------------`);
+
+for (let index = 100; index >=10; index=index-10) {
+    console.log(`${index}`);
+    
+}
