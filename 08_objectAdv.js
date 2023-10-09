60005254ea7162e4b81f9936222e46ee485fc978
payalabhang77 <144082506+payalabhang77@users.noreply.github.com>
2023-10-09 11:04:52 +0530

    object advance
    object advance

---
+++ 08_objectAdv.js
@@ -0,0 +1,73 @@
+
+
+let person = {
+    name: "Jenny",
+    age: 22,
+    isMarried: true,
+    city: "Pune"
+}
+
+const keys = Object.keys(person);
+console.log(keys);
+
+const values = Object.values(person);
+console.log(values);
+
+const entry = Object.entries(person);// [ [], [], [], [] ]
+console.log(entry);
+for (const element of entry) {
+    console.log(element[0], element[1]);
+}
+
+console.log(`=======Traversing of an object...===============`);
+for (const key in person) {
+   console.log(key, person[key]);
+}
+
+const isNameAvailable =  'name' in person;
+console.log(isNameAvailable);
+
+const isPinAvailable = 'pin' in person;
+console.log(isPinAvailable);
+
+const personJenny = {
+    name: "Jenny",
+    age: 22,
+    isMarried: true,
+    city: "Pune"
+}
+personJenny.age = 25;
+console.log(personJenny);
+
+// personJenny = { }; Not allowed
+// personJenny = person; Not allowed
+Object.freeze(personJenny);
+personJenny.city = "Lucknow";
+console.log(personJenny);
+
+console.log(`Object.assign()`);
+
+let eloy = {
+    name: "eloy",
+    age: 54,
+    isMarried: true,
+    country: "USA"
+}
+
+const eloyClone = Object.assign({},eloy)
+console.log(eloyClone);
+
+console.log(`Merging an objects`);
+let stew = {
+    name: "eloy",
+    age: 54,
+    isMarried: true,
+    country: "USA"
+}
+
+const address= {
+    pin: 234567,
+    city: "Austin Texas"
+}
+Object.assign(stew, address);
+console.table(stew);
