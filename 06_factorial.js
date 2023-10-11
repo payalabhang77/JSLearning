122c8e2b490e6dbf67f2dfe8da58810091fad81f
payalabhang77 <144082506+payalabhang77@users.noreply.github.com>
2023-10-11 17:11:23 +0530

    factorial of number
    factorial of number

---
+++ 06_factorial.js
@@ -0,0 +1,22 @@
+// 3! = 1 * 2 * 3 ==> 6
+// 3! = 3 * 2 * 1 ==> 6
+// 4! = 4 * 3 * 2 * 1 ==> 24
+// 5! = 5 * 4 * 3 * 2 * 1 ==> 120 
+
+console.log(`------------------factorial--------------------------`);
+
+function factorial(num){
+    let factNum = 1; // 60 
+    for (let index = num; index >=1; index--) {
+       factNum = factNum * index;
+    }
+    console.log(`Factorial of ${num} is ${factNum} `);
+}
+factorial(5);
+factorial(7);
+factorial(10);
+factorial(9);
+factorial(20);
+
+
+
