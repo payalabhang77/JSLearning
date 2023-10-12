3602f2e41e652791fc70e692c5d55f96ad3db3fe
payalabhang77 <144082506+payalabhang77@users.noreply.github.com>
2023-10-12 16:48:06 +0530

    object cloning
    object cloning

---
+++ 09_objectCloning.js
@@ -0,0 +1,48 @@
+let firstName = "Gajanan";
+let lastName = "Kharat";
+
+let myName = firstName; // Deep Clone
+
+console.log(myName);
+firstName = 'GK';
+console.log(myName);
+
+myName = 'Ganesh';
+
+let person = {
+    name: 'Elon',
+    city: 'Pune',
+    isMarried : true
+}
+let elonPerson = {
+    country: "USA"
+}
+elonPerson = person; // Shallow clone
+
+elonPerson.city= "Mumbai";
+console.log(elonPerson.city); // 
+console.log(person.city);
+
+console.log(`--------deep clone---------------------`);
+
+let stdNagesh = {
+    name: 'Nagesh',
+    age: 22,
+    graduation: "BE - Computer Science"
+}
+
+let stdGanesh = { ...stdNagesh}; // Deep clone using spread operator
+stdGanesh.name = "Ganesh";
+
+console.log(stdNagesh.name);
+
+let stdMangesh = {
+    name: 'Mangesh',
+    age: 22,
+    graduation: "BE - Computer Science"
+}
+
+let stdSuresh = { ...stdMangesh}; // Deep clone using spread operator
+stdSuresh.name = "Ganesh";
+
+console.log(stdMangesh.name);
